1622146cd4b000ae7664ff2195f8ac9da67ebb05
Volodymyr Krolevetskyi <vvkrolevetskiy@gmail.com>
2023-03-14 12:05:20 +0200

    let+if/else

---
+++ Start-JS/index.js
@@ -0,0 +1,49 @@
+let num=2+true;
+console.log(num);//3
+
+
+
+// let userInput = '12345';
+// console.log(userInput);
+// let conv= Number(userInput);
+// console.log(conv);
+
+
+
+// let cat='cat';
+// console.log(cat); 
+
+// let dog='dog';
+// console.log(dog);
+
+// let result = 'cat' > 'dog';
+// console.log(result);
+
+
+
+// let cat= 1;
+// console.log(cat); 
+
+// let dog= 2;
+// console.log(dog); 
+
+// let rat= 4;
+// console.log(dog);
+
+// let animal=cat+dog+rat;
+
+// console.log(animal);
+
+// if (animal>2){
+//     alert('animal');
+// }
+
+
+
+let appleColor= 'green';
+
+if (appleColor==='red'){
+    console.log('apple red');
+} else {
+    console.log('apple color not red');
+}
